06a67b27b5a3fa57f99199bf5fe239d31440448f
Miguel Gonzalez <miguel.glez.cruz@gmail.com>
2023-01-22 17:44:55 +0100

    change baseURL in auth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "deployed-backend-url"
+    ? "https://api.wtwrmgc.students.nomoredomainssbs.ru/"
     : "http://localhost:3001";
 
 // const baseURL = "http://localhost:3001";
